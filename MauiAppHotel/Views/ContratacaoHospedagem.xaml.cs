b688398acfea65c8c44bfefd6d74b2b0d32cb530
costacarlasa <costa.carla.santos@gmail.com>
2025-06-01 18:30:44 -0300

    add View Sobre

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -35,12 +35,7 @@ public partial class ContratacaoHospedagem : ContentPage
 		}
     }
 
-    private void Button_Clicked_1(object sender, EventArgs e)
-    {
-
-    }
-
-    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+      private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
     {
 		DatePicker elemento = sender as DatePicker;
 
@@ -48,5 +43,16 @@ public partial class ContratacaoHospedagem : ContentPage
 
 		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
 		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync( new Sobre());
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
     }
 }
